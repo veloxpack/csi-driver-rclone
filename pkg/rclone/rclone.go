981bde1ee9a197c2bef11e9b7ff047e504baa336
paulgrammer <paulmugaya@live.com>
2025-08-13 12:00:00 +0200

    feat: implement core driver structure and utilities
    - Add main Driver struct with configuration options
    - Implement template variable replacement for dynamic paths
    - Add volume locks for concurrent operation safety
    - Support for PVC/PV metadata substitution

---
+++ pkg/rclone/rclone.go
@@ -0,0 +1,151 @@
+/*
+Copyright 2025 Veloxpack.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rclone
+
+import (
+	"strings"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/rclone/rclone/cmd/mountlib"
+	"github.com/rclone/rclone/vfs/vfscommon"
+	"k8s.io/klog/v2"
+	mount "k8s.io/mount-utils"
+)
+
+const (
+	// DefaultDriverName is the default name of the driver
+	DefaultDriverName = "rclone.csi.veloxpack.io"
+
+	// CSI parameter keys injected by external-provisioner
+	pvcNameKey           = "csi.storage.k8s.io/pvc/name"
+	pvcNamespaceKey      = "csi.storage.k8s.io/pvc/namespace"
+	pvNameKey            = "csi.storage.k8s.io/pv/name"
+	pvcNameMetadata      = "${pvc.metadata.name}"
+	pvcNamespaceMetadata = "${pvc.metadata.namespace}"
+	pvNameMetadata       = "${pv.metadata.name}"
+)
+
+// DriverOptions defines driver parameters specified in driver deployment
+type DriverOptions struct {
+	NodeID             string
+	DriverName         string
+	Endpoint           string
+	RcloneOtherParams  map[string]string
+	RcloneMountOptions *mountlib.Options
+	RcloneVFSOptions   *vfscommon.Options
+}
+
+// Driver is the main driver structure
+type Driver struct {
+	name               string
+	nodeID             string
+	version            string
+	endpoint           string
+	ns                 *NodeServer
+	cscap              []*csi.ControllerServiceCapability
+	nscap              []*csi.NodeServiceCapability
+	volumeLocks        *VolumeLocks
+	rcloneMountOptions *mountlib.Options
+	rcloneVFSOptions   *vfscommon.Options
+	rcloneOtherParams  map[string]string
+}
+
+// NewDriver creates a new driver instance
+func NewDriver(options *DriverOptions) *Driver {
+	klog.V(2).Infof("Driver: %v version: %v", options.DriverName, driverVersion)
+
+	// Initialize rclone logging to redirect to klog
+	InitRcloneLogging()
+
+	d := &Driver{
+		name:               options.DriverName,
+		version:            driverVersion,
+		nodeID:             options.NodeID,
+		endpoint:           options.Endpoint,
+		rcloneVFSOptions:   options.RcloneVFSOptions,
+		rcloneMountOptions: options.RcloneMountOptions,
+		rcloneOtherParams:  options.RcloneOtherParams,
+	}
+
+	d.AddControllerServiceCapabilities([]csi.ControllerServiceCapability_RPC_Type{
+		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
+	})
+
+	d.AddNodeServiceCapabilities([]csi.NodeServiceCapability_RPC_Type{
+		csi.NodeServiceCapability_RPC_UNKNOWN,
+	})
+
+	d.volumeLocks = NewVolumeLocks()
+
+	return d
+}
+
+// NewNodeServer creates a new node server
+func NewNodeServer(d *Driver, mounter mount.Interface) *NodeServer {
+	return &NodeServer{
+		Driver:  d,
+		mounter: mounter,
+	}
+}
+
+// Run starts the CSI driver
+func (d *Driver) Run(testMode bool) {
+	versionMeta, err := GetVersionYAML(d.name)
+	if err != nil {
+		klog.Fatalf("%v", err)
+	}
+	klog.V(2).Infof("\nDRIVER INFORMATION:\n-------------------\n%s\n\nStreaming logs below:", versionMeta)
+
+	mounter := mount.New("")
+	d.ns = NewNodeServer(d, mounter)
+	s := NewNonBlockingGRPCServer()
+	s.Start(d.endpoint,
+		NewDefaultIdentityServer(d),
+		NewControllerServer(d),
+		d.ns,
+		testMode)
+	s.Wait()
+}
+
+// AddControllerServiceCapabilities adds controller service capabilities
+func (d *Driver) AddControllerServiceCapabilities(cl []csi.ControllerServiceCapability_RPC_Type) {
+	csc := make([]*csi.ControllerServiceCapability, 0, len(cl))
+	for _, c := range cl {
+		csc = append(csc, NewControllerServiceCapability(c))
+	}
+	d.cscap = csc
+}
+
+// AddNodeServiceCapabilities adds node service capabilities
+func (d *Driver) AddNodeServiceCapabilities(nl []csi.NodeServiceCapability_RPC_Type) {
+	nsc := make([]*csi.NodeServiceCapability, 0, len(nl))
+	for _, n := range nl {
+		nsc = append(nsc, NewNodeServiceCapability(n))
+	}
+	d.nscap = nsc
+}
+
+// replaceWithMap replaces template variables in str with values from the map
+// This enables dynamic path substitution using PVC/PV metadata
+func replaceWithMap(str string, m map[string]string) string {
+	for k, v := range m {
+		if k != "" {
+			str = strings.ReplaceAll(str, k, v)
+		}
+	}
+	return str
+}
