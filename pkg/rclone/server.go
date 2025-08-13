e757adfa70d529c6a5a4fcbf652e8c3616bfe933
paulgrammer <paulmugaya@live.com>
2025-08-13 11:30:00 +0200

    feat: implement gRPC server infrastructure
    - Add non-blocking gRPC server implementation
    - Support for CSI Identity, Controller, and Node services
    - Include proper error handling and logging

---
+++ pkg/rclone/server.go
@@ -0,0 +1,130 @@
+/*
+Copyright 2025 Veloxpack.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rclone
+
+import (
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc"
+	"k8s.io/klog/v2"
+)
+
+// NonBlockingGRPCServer defines Non blocking GRPC server interfaces
+type NonBlockingGRPCServer interface {
+	// Start services at the endpoint
+	Start(endpoint string, ids csi.IdentityServer, cs csi.ControllerServer, ns csi.NodeServer, testMode bool)
+	// Wait for the service to stop
+	Wait()
+	// Stop the service gracefully
+	Stop()
+	// ForceStop stops the service forcefully
+	ForceStop()
+}
+
+// NewNonBlockingGRPCServer creates a new NonBlockingGRPCServer
+func NewNonBlockingGRPCServer() NonBlockingGRPCServer {
+	return &nonBlockingGRPCServer{}
+}
+
+// nonBlockingGRPCServer is the implementation of NonBlockingGRPCServer
+type nonBlockingGRPCServer struct {
+	wg     sync.WaitGroup
+	server *grpc.Server
+}
+
+// Start starts the gRPC server
+//
+//nolint:lll
+func (s *nonBlockingGRPCServer) Start(endpoint string, ids csi.IdentityServer, cs csi.ControllerServer, ns csi.NodeServer, testMode bool) {
+	s.wg.Add(1)
+	go s.serve(endpoint, ids, cs, ns, testMode)
+}
+
+// Wait waits for the server to complete
+func (s *nonBlockingGRPCServer) Wait() {
+	s.wg.Wait()
+}
+
+// Stop stops the gRPC server gracefully
+func (s *nonBlockingGRPCServer) Stop() {
+	s.server.GracefulStop()
+}
+
+// ForceStop stops the gRPC server forcefully
+func (s *nonBlockingGRPCServer) ForceStop() {
+	s.server.Stop()
+}
+
+// serve starts the gRPC server and registers the services
+//
+//nolint:lll
+func (s *nonBlockingGRPCServer) serve(endpoint string, ids csi.IdentityServer, cs csi.ControllerServer, ns csi.NodeServer, testMode bool) {
+	proto, addr, err := ParseEndpoint(endpoint)
+	if err != nil {
+		klog.Fatal(err.Error())
+	}
+
+	if proto == "unix" {
+		addr = "/" + addr
+		if err := os.Remove(addr); err != nil && !os.IsNotExist(err) {
+			klog.Fatalf("Failed to remove %s, error: %v", addr, err)
+		}
+	}
+
+	listener, err := net.Listen(proto, addr)
+	if err != nil {
+		klog.Fatalf("Failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(logGRPC),
+	}
+	server := grpc.NewServer(opts...)
+	s.server = server
+
+	if ids != nil {
+		csi.RegisterIdentityServer(server, ids)
+	}
+	if cs != nil {
+		csi.RegisterControllerServer(server, cs)
+	}
+	if ns != nil {
+		csi.RegisterNodeServer(server, ns)
+	}
+
+	// Used to stop the server while running tests
+	if testMode {
+		s.wg.Done()
+		go func() {
+			// make sure Serve() is called
+			s.wg.Wait()
+			time.Sleep(time.Millisecond * 1000)
+			s.server.GracefulStop()
+		}()
+	}
+
+	klog.Infof("Listening for connections on address: %#v", listener.Addr())
+
+	err = server.Serve(listener)
+	if err != nil {
+		klog.Fatalf("Failed to serve grpc server: %v", err)
+	}
+}
