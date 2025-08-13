86aae7051fbb0ea1c17e1c95c9a0dabf9e410698
paulgrammer <paulmugaya@live.com>
2025-08-13 18:00:00 +0200

    feat: add minimal backend support for smaller images
    - Include only essential rclone backends (S3, GCS, Azure, etc.)
    - Reduce image size and startup time
    - Support for most common cloud storage providers

---
+++ pkg/rclone/backends_minimal.go
@@ -0,0 +1,36 @@
+//go:build minimal
+
+/*
+Copyright 2025 Veloxpack.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rclone
+
+import (
+	_ "github.com/rclone/rclone/backend/azureblob"
+	_ "github.com/rclone/rclone/backend/b2"
+	_ "github.com/rclone/rclone/backend/box"
+	_ "github.com/rclone/rclone/backend/drive"
+	_ "github.com/rclone/rclone/backend/dropbox"
+	_ "github.com/rclone/rclone/backend/ftp"
+	_ "github.com/rclone/rclone/backend/googlecloudstorage"
+	_ "github.com/rclone/rclone/backend/local"
+	_ "github.com/rclone/rclone/backend/onedrive"
+	_ "github.com/rclone/rclone/backend/s3"
+	_ "github.com/rclone/rclone/backend/sftp"
+	_ "github.com/rclone/rclone/backend/swift"
+	_ "github.com/rclone/rclone/backend/webdav"
+	_ "github.com/rclone/rclone/cmd/mount2"
+)
